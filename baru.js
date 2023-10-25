da7847f9dff7324dd29d6d9cb04b285e8c90f7dd
destro <mrdestroyers2002@gmail.com>
2023-10-25 14:54:00 +0700

    buat tombol .forMobile untuk home.html menggunakan forMobile.js
    seperti pada contoh
    1. baru.html
    2. baru.css
    dan 3. baru.js

---
+++ baru.js
@@ -0,0 +1,17 @@
+// const openBtn = document.getElementById("openModal");
+// const closeBtn = document.getElementById("closeModal");
+// const modal = document.getElementById("modal")
+
+// openBtn.addEventListener("click", () => {
+//     modal.classList.add("open");
+// });
+
+// openBtn.addEventListener("click", () => {
+//     modal.classList.remove("open");
+// });
+
+// onclick="document.getElementById('demo').innerHTML = Date()
+function why() {
+    document.getElementById("modalInner").style.display = "none";
+
+}
