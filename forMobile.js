bbdee1bcbb2be3179b43416bb6056cc5d636ca9c
destro <mrdestroyers2002@gmail.com>
2023-10-31 15:27:02 +0700

    Per rapi search-field di side-mobile-nav-bar

--- forMobile.js
+++ forMobile.js
@@ -1,10 +1,11 @@
 function buttonList () {
-    document.getElementById("mobile-nav-bar").style.display = "grid";
+    document.getElementById("wrapper-mobile-nav-bar").style.display = "grid";
     // document.getElementById("theBody").style.background="rgba(0,0,0, 0.3";
-    document.getElementById("serch-engine").style.display = "none";
-    document.getElementById("search-field").style.display = "none";
+    document.getElementById("search-engine-2").style.display = "none";
+    // document.getElementById("search-field").style.display = "none";
 }
 
 function sideBarExit(){
-    document.getElementById("mobile-nav-bar").style.display = "none";
+    document.getElementById("wrapper-mobile-nav-bar").style.display = "none";
+    document.getElementById("search-engine-2").style.display = "flex";
 }
